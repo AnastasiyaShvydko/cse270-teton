220ee6e02f58ad4fdb3928b170a4bd2a8d7208a6
AnastasiyaShvydko <84057111+AnastasiyaShvydko@users.noreply.github.com>
2025-02-18 20:18:02 -0500

    Update test_smokeTest.py

--- tests/test_smokeTest.py
+++ tests/test_smokeTest.py
@@ -12,7 +12,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
 
 class TestSmokeTest():
-  def setup_method(self, method):
+  def setup_method(self,method):
     options = Options()
     options.add_argument("--headless=new")
     self.driver = webdriver.Chrome(options=options)
